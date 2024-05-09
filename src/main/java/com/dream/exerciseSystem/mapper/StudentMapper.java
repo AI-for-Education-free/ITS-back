e701becab8f917d3ede112fc8497536c7a9feb18
Ulysee <747137135@qq.com>
2024-05-09 22:15:01 +0800

    2024.5.9 22:14 Add StudentMapper

---
+++ src/main/java/com/dream/exerciseSystem/mapper/StudentMapper.java
@@ -0,0 +1,15 @@
+package com.dream.exerciseSystem.mapper;
+
+import com.dream.exerciseSystem.domain.Student;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author xzy
+* @description 针对表【student(student model for mybatis)】的数据库操作Mapper
+* @createDate 2024-05-09 13:52:05
+* @Entity generator.domain.Student
+*/
+public interface StudentMapper extends BaseMapper<Student> {
+
+
+}
